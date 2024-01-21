7a0cbb3686cd052143e6f3067cfeb04d8d57b5ff
chamox <sezuniga1@uc.cl>
2024-01-21 17:12:09 -0300

    Add final version of project 02

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,9 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    // console.log('effect', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      // console.log('handle move', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      // console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <h3>Mouse follower</h3>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'OFF' : 'ON'}
+        </button>
+      </div>
+    </main>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h3>Mouse follower</h3>
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
 }
 
 export default App
