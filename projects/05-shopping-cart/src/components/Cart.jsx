95d1d0186b505e0ba65eeb97e1ce8cc425facab9
chamox <sezuniga1@uc.cl>
2024-02-13 00:32:21 -0300

    Add Cart

--- projects/05-shopping-cart/src/components/Cart.jsx
+++ projects/05-shopping-cart/src/components/Cart.jsx
@@ -1,9 +1,29 @@
 import { useId } from 'react'
 import { CartIcon, ClearCartIcon } from './Icons'
+import { useCart } from '../hooks/useCart'
 import './Cart.css'
+
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img
+        src={thumbnail}
+        alt={title}
+      />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
 
 export function Cart() {
   const cartCheckBoxId = useId()
+  const { clearCart, cart, addToCart } = useCart()
 
   return (
     <>
@@ -21,21 +41,15 @@ export function Cart() {
 
       <aside className="cart">
         <ul>
-          <li>
-            <img
-              src="https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-              alt="iphone"
+          {cart.map((product) => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
             />
-            <div>
-              <strong>Iphone</strong> - $899
-            </div>
-            <footer>
-              <small>Qty: 1</small>
-              <button>+</button>
-            </footer>
-          </li>
+          ))}
         </ul>
-        <button>
+        <button onClick={clearCart}>
           <ClearCartIcon />
         </button>
       </aside>
